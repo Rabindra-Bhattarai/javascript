a577fa447a923121744e33ed8098f25b9c836dc7
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2025-11-03 09:20:26 +0545

    push

---
+++ class2.js
@@ -0,0 +1,14 @@
+//fucntion
+function greet(name){
+    return "Hello ", + name
+}
+
+let first = greet("Hari")
+console.log(first)
+
+//no restriction of argument and return
+function greetNormal(){
+    console.log("Hello")
+}
+
+greetNormal()
